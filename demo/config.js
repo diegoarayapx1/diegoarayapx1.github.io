413adafb9591115b1cdf7d7b01bc760d02187735
Diego Araya <diegoarx1@gmail.com>
2026-09-24 14:35:36 -0300

    Update config.js
    test1

--- demo/config.js
+++ demo/config.js
@@ -13,7 +13,7 @@ window.LOCAL = {
 
   // Place ID de Google del local (botón de reseña). Se busca en:
   // https://developers.google.com/maps/documentation/javascript/examples/places-placeid-finder
-  googlePlaceId: "",
+  googlePlaceId: "ChIJIXAoIb7aYpYRTr6jOgw-xnM",
 
   // Móvil chileno. Sirve "+56 9 1234 5678", "912345678" o "56912345678".
   whatsapp: "",
@@ -24,7 +24,7 @@ window.LOCAL = {
   tiktok: "",
 
   // Dirección o nombre del lugar para "Cómo llegar".
-  mapa: "Plaza de Armas, Santiago",
+  mapa: "",
 
   // Datos de transferencia. Si "titular" queda vacío, no aparece el botón.
   transferencia: {
